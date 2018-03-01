1a150dc4f0bce8dbc048d6543bf93dbfed1d3c64
Christoff Smith <Christoff.Smith@standardbank.co.za>
2018-03-01 20:03:41 +0200

    Added data files

--- src/main/java/com/team/afk/googlehashcode/AfkGoogleHashcodeApplication.java
+++ src/main/java/com/team/afk/googlehashcode/AfkGoogleHashcodeApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AfkGoogleHashcodeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AfkGoogleHashcodeApplication.class, args);
+		//SpringApplication.run(AfkGoogleHashcodeApplication.class, args);
+
+		System.out.println("Test");
+
 	}
 }
