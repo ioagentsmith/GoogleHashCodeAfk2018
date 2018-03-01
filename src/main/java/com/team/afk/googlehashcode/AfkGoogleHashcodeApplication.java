fb442d5193905d69cf88e27a0e05bec4575e69cc
Christoff Smith <Christoff.Smith@standardbank.co.za>
2018-03-01 19:11:37 +0200

    Initial commit

---
+++ src/main/java/com/team/afk/googlehashcode/AfkGoogleHashcodeApplication.java
@@ -0,0 +1,12 @@
+package com.team.afk.googlehashcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfkGoogleHashcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfkGoogleHashcodeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/team/afk/googlehashcode/AfkGoogleHashcodeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.team.afk.googlehashcode;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AfkGoogleHashcodeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
